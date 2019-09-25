0ea916bd0cf1f6bb20d1e200007603e8c4221db8
Don Button <donpublic@digitalgizmo.com>
2019-09-25 16:42:37 -0400

    two image galleries

--- js/scroll-wapo1.js
+++ js/scroll-wapo1.js
@@ -1,47 +1,105 @@
-// Hide all but first image
-$(".image-panel").children().each(function(i) {
-	// Skip first image -- it's a pin, not a transition
-	if (i > 0) {
+
+var ctrl = new ScrollMagic.Controller();
+
+// ----- Chapter 1 ----
+// Hide all but title and first image
+$("#image-sequence1").children().each(function(i) {
+	// Skip fist child, the title, and first image -- it's a pin, not a transition
+	if (i > 1) {
 		TweenMax.set(this, {autoAlpha:0});
 	}
 });
 
-var ctrl = new ScrollMagic.Controller();
-
 // Set pin for Image container
 var containerScene = new ScrollMagic.Scene({
-	// triggerElement: '.container0',
-	// triggerElement: '#container1',
-	// triggerElement: "#chapter1-trigger", // point of execution
-	triggerElement: ".image-panel", // point of execution
-	// duration: 3500, // was 1300
-	duration: $(".caption-panel").height(),
+	triggerElement: "#image-sequence1", // point of execution
+	duration: $("#caption-sequence1").height(),
 	triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
-	// offset: -500 // was 320
 })
-.setPin(".image-panel", {pushFollowers: false})
-// .setPin(".image-panel")
+.setPin("#image-sequence1", {pushFollowers: false})
+// .setPin("#image-sequence1")
 .addIndicators()
 .addTo(ctrl);
 
-// Set tweens for dissolves
-var targetPrev = $(".image-panel").children().eq(0);
-let target = $(".image-panel").children().eq(1);
-var tl = new TimelineMax();
-tl.to(target, 2, {autoAlpha:1})
-	.to(targetPrev, 1, {autoAlpha:0});
-// var tween = TweenMax.staggerTo("target", 1, {autoAlpha:1}, 1);	
+
+// Create scenes
+// Loop through all divs in text-panel
+$("#caption-sequence1").children().each(function(i) {
+	// Skip first image -- it's a pin, not a transition
+	if (i > 0) {
+		// Current target is +1 because 1st div in image panel is title
+		var targetPrev = $("#image-sequence1").children().eq(i);
+		let target = $("#image-sequence1").children().eq(i + 1);
+
+		var tl = new TimelineMax();
+		// Base transitin condition class name
+		// "this" is not a jQuery object, so need straight JS
+		tl.to(target, 2, {autoAlpha:1})
+			.to(targetPrev, 1, {autoAlpha:0});			
+
+		new ScrollMagic.Scene({
+			triggerElement: this,
+			duration: 350, // pin the element for a total of 400px
+			triggerHook: .8, // trigger low in the viewport
+		})
+		.setTween(tl)
+		// .on('start', function() {
+		// 	console.log("-- targPrev: " + i + " target: " + (i+1));
+		// })
+		.addIndicators()
+		.addTo(ctrl);
+	} // end if i > 0
+});
 
 
-// Scene2 first dissolve
-var scene2 = new ScrollMagic.Scene({
-  triggerElement: "#container2", // point of execution
-  duration: 250 // pin the element for a total of 400px
+// ----- Chapter 2 ----
+// Hide all but title and first image
+$("#image-sequence2").children().each(function(i) {
+	// Skip fist child, the title, and first image -- it's a pin, not a transition
+	if (i > 1) {
+		TweenMax.set(this, {autoAlpha:0});
+	}
+});
+// Set pin for Image container
+var containerScene = new ScrollMagic.Scene({
+	triggerElement: "#image-sequence2", // point of execution
+	duration: $("#caption-sequence2").height(),
+	triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
 })
-// .setTween(tween)
-.setTween(tl)
-// .on('start', function() {
-// 	console.log("container2 trigger");
-// })
+.setPin("#image-sequence2", {pushFollowers: false})
+// .setPin("#image-sequence1")
 .addIndicators()
 .addTo(ctrl);
+
+
+// Create scenes
+// Loop through all divs in text-panel
+$("#caption-sequence2").children().each(function(i) {
+	// Skip first image -- it's a pin, not a transition
+	if (i > 0) {
+		// Current target is +1 because 1st div in image panel is title
+		var targetPrev = $("#image-sequence2").children().eq(i);
+		let target = $("#image-sequence2").children().eq(i + 1);
+
+		var tl = new TimelineMax();
+		// Base transitin condition class name
+		// "this" is not a jQuery object, so need straight JS
+		tl.to(target, 2, {autoAlpha:1})
+			.to(targetPrev, 1, {autoAlpha:0});			
+
+		new ScrollMagic.Scene({
+			triggerElement: this,
+			duration: 350, // pin the element for a total of 400px
+			triggerHook: .8, // trigger low in the viewport
+		})
+		.setTween(tl)
+		.on('start', function() {
+			console.log("-- Chap2 targPrev: " + i + " target: " + (i+1));
+		})
+		.addIndicators()
+		.addTo(ctrl);
+	} // end if i > 0
+});
+
+
+
