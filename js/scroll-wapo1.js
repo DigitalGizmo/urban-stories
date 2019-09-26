385b6214cd838d78d8430406a761ec86ad6b73ff
Don Button <donpublic@digitalgizmo.com>
2019-09-26 12:59:03 -0400

    pinned chapter titles

--- js/scroll-wapo1.js
+++ js/scroll-wapo1.js
@@ -5,16 +5,28 @@ var ctrl = new ScrollMagic.Controller();
 // Hide all but title and first image
 $("#image-sequence1").children().each(function(i) {
 	// Skip fist child, the title, and first image -- it's a pin, not a transition
-	if (i > 1) {
+	if (i > 0) {
 		TweenMax.set(this, {autoAlpha:0});
 	}
 });
 
-// Set pin for Image container
+// Set pin for first chapter TITLE
+var containerScene = new ScrollMagic.Scene({
+	triggerElement: "#chapter1-trigger", // point of execution
+	duration: $("#chapter1").height(),
+	triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
+})
+.setPin("#chapter1-trigger", {pushFollowers: false})
+// .setPin("#image-sequence1")
+.addIndicators()
+.addTo(ctrl);
+
+// Set pin for first GALLERY Image container
 var containerScene = new ScrollMagic.Scene({
 	triggerElement: "#image-sequence1", // point of execution
 	duration: $("#caption-sequence1").height(),
 	triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
+	offset: -50 // was 320
 })
 .setPin("#image-sequence1", {pushFollowers: false})
 // .setPin("#image-sequence1")
@@ -28,8 +40,8 @@ $("#caption-sequence1").children().each(function(i) {
 	// Skip first image -- it's a pin, not a transition
 	if (i > 0) {
 		// Current target is +1 because 1st div in image panel is title
-		var targetPrev = $("#image-sequence1").children().eq(i);
-		let target = $("#image-sequence1").children().eq(i + 1);
+		var targetPrev = $("#image-sequence1").children().eq(i -1);
+		let target = $("#image-sequence1").children().eq(i);
 
 		var tl = new TimelineMax();
 		// Base transitin condition class name
@@ -60,11 +72,25 @@ $("#image-sequence2").children().each(function(i) {
 		TweenMax.set(this, {autoAlpha:0});
 	}
 });
+
+// Set pin for 2nd chapter TITLE
+var containerScene = new ScrollMagic.Scene({
+	triggerElement: "#chapter2-trigger", // point of execution
+	duration: $("#chapter2").height(),
+	triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
+})
+.setPin("#chapter2-trigger", {pushFollowers: false})
+// .setPin("#image-sequence1")
+.addIndicators()
+.addTo(ctrl);
+
+
 // Set pin for Image container
 var containerScene = new ScrollMagic.Scene({
 	triggerElement: "#image-sequence2", // point of execution
 	duration: $("#caption-sequence2").height(),
 	triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
+	offset: -50 // was 320
 })
 .setPin("#image-sequence2", {pushFollowers: false})
 // .setPin("#image-sequence1")
